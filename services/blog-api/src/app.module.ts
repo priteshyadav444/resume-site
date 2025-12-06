b68d56cd64fa7d5c7082d101d2ad332e9d955fe9
Pritesh Yadav <priteshyadav2015@gmail.com>
2025-12-06 15:59:17 +0530

    feat(blog-api): update dependencies and integrate Apollo Driver for GraphQL
    - Added @nestjs/apollo and apollo-server-express to enhance GraphQL capabilities.
    - Updated GraphQLModule configuration to use ApolloDriver.
    - Retained existing NestJS dependencies while ensuring compatibility.
    - Removed outdated sitemap.xml file as part of project cleanup.

--- services/blog-api/src/app.module.ts
+++ services/blog-api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { BlogModule } from './blog/blog.module';
@@ -9,7 +10,8 @@ import { MenuModule } from './menu/menu.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
       playground: true,
